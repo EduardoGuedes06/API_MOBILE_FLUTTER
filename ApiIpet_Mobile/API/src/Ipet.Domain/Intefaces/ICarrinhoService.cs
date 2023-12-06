282b01a67986951c66ab621494af392dd6c2a45f
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2023-12-06 16:36:58 -0300

    first commit

---
+++ ApiIpet_Mobile/API/src/Ipet.Domain/Intefaces/ICarrinhoService.cs
@@ -0,0 +1,12 @@
+﻿using Ipet.Domain.Models;
+
+namespace Ipet.Interfaces.Services
+{
+    public interface ICarrinhoService
+    {
+        Task AtualizarQuantidadeProduto(Guid CarrinhoId, int novaQuantidade);
+        Task CriarCarrinho(Carrinho carrinho);
+        Task<IEnumerable<Carrinho>> ObterCarrinhoPorUsuario(Guid usuarioId);
+        Task RemoverCarrinho(Guid carrinhoId);
+    }
+}
